08e087c239b4812411bd480ed3a2117ef6edac0e
Kevin Schmitz <31090122+Th3red@users.noreply.github.com>
2025-01-04 18:27:38 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Set the rule to 'warn' instead of 'error'
+      //"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      
+      // Alternatively, to disable the rule entirely
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
